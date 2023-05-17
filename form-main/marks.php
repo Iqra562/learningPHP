4dfb30e4ae40ae8930bb932137944e57ddb816e4
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2023-05-17 12:48:42 +0500

    uncompleted form

---
+++ form-main/marks.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap-4.5.3-dist/css/bootstrap.min.css">
+ 
+</head>
+<body>
+ <div class="container">
+    <form action="result.php" method="POST" >
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="">Name</label>
+                    <input type="text" name="nameStd" class="form-control">
+                    
+                </div>
+                <div class="form-group">
+                    <label for="">Father Name</label>
+                    <input type="text" name="fatherName" class="form-control">
+                    
+                </div>
+                <div class="form-group">
+                    <label for="">Class</label>
+                    <input type="text" name="class" class="form-control">
+
+                </div>
+                <div class="form-group">
+                    <label for="">Roll Number</label>
+                    <input type="text" name="rollNumber" class="form-control">
+                    
+                </div>
+                  <div class="form-group">
+    <label for="">Biology</label>
+    <input type="text" name="biology" class="form-control">
+
+   </div>
+   <div class="form-group">
+    <label for="">Chemistry</label>
+    <input type="text" name="chemistry" class="form-control">
+
+   </div>
+   <div class="form-group">
+    <label for="">Physics</label>
+    <input type="text" name="Physics" class="form-control">
+
+   </div>
+   <div class="form-group">
+    <label for="">Mathematics</label>
+    <input type="text" name="Mathematics" class="form-control">
+
+   </div>
+
+   </div>
+   <div class="col-md-6">
+
+  
+   <div class="form-group">
+    <label for="">Computer</label>
+    <input type="text" name="Computer" class="form-control">
+
+   </div>
+   
+
+   <div class="form-group">
+    <label for="">English</label>
+    <input type="text" name="English" class="form-control">
+
+   </div>
+   <div class="form-group">
+    <label for="">Sindhi</label>
+    <input type="text" name="Sindhi" class="form-control">
+
+   </div>
+   <div class="form-group">
+    <label for="">Urdu</label>
+    <input type="text" name="Urdu" class="form-control">
+
+   </div>
+   <div class="form-group">
+    <label for="">Bio Practical</label>
+    <input type="text" name="BioPractical"  class="form-control">
+
+   </div>
+   <div class="form-group">
+    <label for="">Physics Practical</label>
+    <input type="text" name="PhysicsPractical"  class="form-control">
+
+   </div>
+   <div class="form-group">
+    <label for="">Chemistry Practical</label>
+    <input type="text" name="ChemistryPractical"  class="form-control">
+
+   </div>
+   </div>
+
+   </div>
+   <input type="submit" class="btn btn-info" name="formInfo">
+   </form>
+   </div>
+</body>
+</html>
