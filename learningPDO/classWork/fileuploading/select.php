6227de6dbed6b159bd90d989976061ca3844b903
Iqra562 <iqra44938@gmail.com>
2023-06-03 17:04:40 +0500

    login

--- learningPDO/classWork/fileuploading/select.php
+++ learningPDO/classWork/fileuploading/select.php
@@ -7,25 +7,26 @@ include("connections.php")
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../bootstrap.min.css">
     <title>Document</title>
 </head>
 <body>
     <div class="container">
         <div class="row">
             <?php
-            $query  = $pdo-> query("select * from products");
-            
+            $query  = $pdo->query("select * from products");
             $result = $query->fetchAll(PDO::FETCH_ASSOC);
             foreach($result as $data){
-
                 ?>
                 <div class="col-md-3">
-                    <div class="card" style="width: 18rem;">
-                <div class="card-img-top" src="images/<?php echo $data['p_image']?>"></div>
+                    <div class="card" >
+                <img class="card-img-top" src="images/<?php echo $data['p_image']?>" alt="">
                 <div class="card-body">
-                    <div class="card-title"><?php echo $data['pro_name']?></div>
+                <div class="card-title"><?php echo $data['pro_name']?>
                 </div>
                 </div>
+                </div>
+            </div>
                 <?php
             } 
                 ?>
---
+++ login/login.php
@@ -0,0 +1,38 @@
+<?php
+// session_start();
+include("query.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap.min.css">
+
+</head>
+<body>
+    <div class="container">
+        <form action="" method= "post">
+      <div class="form-group">
+        <label for="">Name</label>
+        <input type="text" name="name" id=""  class="form-control">
+        
+      </div>
+      <!-- <div class="form-group">
+        <label for="">Email</label>
+        <input type="text" name="email" id="" class="form-control">
+        
+      </div> -->
+      <div class="form-group">
+        <label for="">Password</label>
+        <input type="text" name="password" id=""  class="form-control">
+        
+      </div>
+      <input type="submit" name="login" id="" value = "Add" class="btn btn-primary">
+        </form>
+    </div>
+</body>
+</html>
