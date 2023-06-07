4e7f476eec4c4319ac6f11b461d6a58254a482af
Iqra <iqra44938@gmail.com>
2023-06-07 17:14:58 +0500

    learning

--- learningPDO/classWork/fileuploading/connections.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$server = "mysql:host=localhost;dbname=daraz";
-$user = "root";
-$password = "";
-
-$pdo= new PDO($server,$user,$password);
-
-?>
--- learningPDO/fileuploading/connections.php
+++ learningPDO/fileuploading/connections.php
@@ -1,6 +1,9 @@
 <?php
+session_start();
 $server = "mysql:host=localhost;dbname=daraz";
 $user = "root";
 $password = "";
-$pdo= new PDO($server, $user, $password);
+
+$pdo= new PDO($server,$user,$password);
+
 ?>
